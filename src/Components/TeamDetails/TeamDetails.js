f71d8d8c66843e828084cef07651b70720b1a30e
Sakib Noman <skbnmn@gmail.com>
2021-03-11 19:13:28 +0600

    set logo instead of badge, redirect added

--- src/Components/TeamDetails/TeamDetails.js
+++ src/Components/TeamDetails/TeamDetails.js
@@ -19,6 +19,7 @@ const TeamDetails = () => {
 
     //destructuring data from league object
     const { strGender, strSport, strCountry, intFormedYear, strDescriptionEN, strTwitter, strYoutube, strFacebook } = league;
+    console.log(league);
 
     //useEffect hook to load json data
     useEffect(() => {
